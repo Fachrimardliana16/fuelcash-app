10fd0327ea69f10b20b049ba836a0da38b909c1a
Fachri Mardliana <fachrimardliana16@gmail.com>
2025-03-08 20:55:46 +0700

    fix: Update balance input to use numeric input mode and improve balance calculation; modify footer text for clarity

--- resources/views/components/filament/footer.blade.php
+++ resources/views/components/filament/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center px-4 py-3">
     <p class="text-sm text-gray-500">
-        © {{ date('Y') }} FuelCash-App. Perumdam Tirta Perwira. Develop by Department IT versi 1.1 2025. All rights
+        © {{ date('Y') }} FuelCash-App v1.1 | Perumdam Tirta Perwira | Developed by IT Department | All rights
         reserved.
     </p>
 </div>
